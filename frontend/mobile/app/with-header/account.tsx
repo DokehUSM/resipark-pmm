efcbee7434bd26b151922820802b28344b55e00e
DokehUSM <diego.prokes@usm.cl>
2025-09-25 01:11:28 -0300

    front mobile: quitar enlaces de cambiar contraseña y Olvidaste tu contrasena?

--- frontend/mobile/app/with-header/account.tsx
+++ frontend/mobile/app/with-header/account.tsx
@@ -31,9 +31,9 @@ export default function Account() {
       </TouchableOpacity>
 
       {/* Link Cambiar contraseña */}
-      <TouchableOpacity>
+      {/* <TouchableOpacity>
         <Text style={styles.link}>Cambiar contraseña</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 }
